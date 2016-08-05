d5f09ae5f6b1c319129500a1e894e1e2cb2e3bd5
Dénes Rolya <drolya@chemaxon.com>
2016-08-05 18:16:52 +0200

    feat(gmaps): display maps on category entry page

--- www/js/categories/controllers/entry.view.js
+++ www/js/categories/controllers/entry.view.js
@@ -5,10 +5,31 @@
         .module('app')
         .controller('EntryViewController', EntryViewController);
 
-    EntryViewController.$inject = [];
-    function EntryViewController() {
+    EntryViewController.$inject = ['$scope'];
+    function EntryViewController($scope) {
         var vm = this;
 
-        
+        google.maps.event.addDomListener(window, 'load', function() {
+            var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+                var myLocation = new google.maps.Marker({
+                    position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                    map: map,
+                    title: "My Location"
+                });
+            });
+
+            $scope.map = map;
+        });
     }
 })();
